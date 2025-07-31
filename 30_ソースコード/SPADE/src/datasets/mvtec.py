04f0f0955b5f4d848515667541e02e097a7d5f16
dhia <furafuradoru594@gmail.com>
2025-07-31 20:48:59 +0900

    解釈付け加え

--- 30_ソースコード/SPADE/src/datasets/mvtec.py
+++ 30_ソースコード/SPADE/src/datasets/mvtec.py
@@ -18,6 +18,7 @@ CLASS_NAMES = ['bottle', 'cable', 'capsule', 'carpet', 'grid',
 class MVTecDataset(Dataset):
     def __init__(self, root_path='../data', class_name='bottle', is_train=True,
                  resize=256, cropsize=224):
+        #    プログラムの前提条件をチェックするための**アサーション（Assertion）満たしていない場合メッセージとAssertionError**
         assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
         self.root_path = root_path
         self.class_name = class_name
@@ -29,7 +30,7 @@ class MVTecDataset(Dataset):
         # download dataset if not exist
         self.download()
 
-        # load dataset
+        # load dataset パスを指定 画像、ラベル、マスクのパスリスト
         self.x, self.y, self.mask = self.load_dataset_folder()
 
         # set transforms
@@ -41,7 +42,7 @@ class MVTecDataset(Dataset):
         # self.transform_mask = T.Compose([T.Resize(resize, Image.NEAREST),
         #                                  T.CenterCrop(cropsize),
         #                                  T.ToTensor()])
-        self.transform_x = T.Compose([T.Resize(resize, T.InterpolationMode.LANCZOS), # <- ANTIALIASの新しい書き方
+        self.transform_x = T.Compose([T.Resize(resize, T.InterpolationMode.LANCZOS), # <- ANTIALIASの新しい書き方 size, method
                               T.CenterCrop(cropsize),
                               T.ToTensor(),
                               T.Normalize(mean=[0.485, 0.456, 0.406],
@@ -53,12 +54,14 @@ class MVTecDataset(Dataset):
 
 
     def __getitem__(self, idx):
+        # この段階ではまだパスを代入しているだけ
         x, y, mask = self.x[idx], self.y[idx], self.mask[idx]
 
+        # ファイルパスを使って、画像ファイル画像を読み込み、変換を適用
         x = Image.open(x).convert('RGB')
         x = self.transform_x(x)
 
-        if y == 0:
+        if y == 0: #正常画像ならばマスクは不要なので黒塗り
             mask = torch.zeros([1, self.cropsize, self.cropsize])
         else:
             mask = Image.open(mask)
@@ -80,6 +83,7 @@ class MVTecDataset(Dataset):
         img_dir = os.path.join(self.mvtec_folder_path, self.class_name, phase)
         gt_dir = os.path.join(self.mvtec_folder_path, self.class_name, 'ground_truth')
 
+        #フォルダ内にある全てのファイル名とサブディレクトリ名をリストとして返し、ソート
         img_types = sorted(os.listdir(img_dir))
         for img_type in img_types:
 
@@ -89,8 +93,8 @@ class MVTecDataset(Dataset):
                 continue
             img_fpath_list = sorted([os.path.join(img_type_dir, f)
                                      for f in os.listdir(img_type_dir)
-                                     if f.endswith('.png')])
-            x.extend(img_fpath_list)
+                                     if f.endswith('.png')])  # 画像ファイルの拡張子がpngのもの
+            x.extend(img_fpath_list) #既存のリストxの末尾に追加
 
             # load gt labels
             if img_type == 'good':
@@ -98,7 +102,10 @@ class MVTecDataset(Dataset):
                 mask.extend([None] * len(img_fpath_list))
             else:
                 y.extend([1] * len(img_fpath_list))
-                gt_type_dir = os.path.join(gt_dir, img_type)
+                gt_type_dir = os.path.join(gt_dir, img_type)#特定のマスク画像群パス
+
+                #フルパスからファイル名と拡張子部分だけを取り出しbasenameで、splittext "split extension"（拡張子を分割してリストにその０要素目）
+
                 img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in img_fpath_list]
                 gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
                                  for img_fname in img_fname_list]
@@ -116,8 +123,8 @@ class MVTecDataset(Dataset):
             if not os.path.exists(tar_file_path):
                 download_url(URL, tar_file_path)
             print('unzip downloaded dataset: %s' % tar_file_path)
-            tar = tarfile.open(tar_file_path, 'r:xz')
-            tar.extractall(self.mvtec_folder_path)
+            tar = tarfile.open(tar_file_path, 'r:xz')  #レトルトカレーのパウチを開ける
+            tar.extractall(self.mvtec_folder_path)   #指定されたフォルダに展開
             tar.close()
 
         return
@@ -133,3 +140,10 @@ class DownloadProgressBar(tqdm):
 def download_url(url, output_path):
     with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
+# # withを使わない場合（手動での後片付けが必要）
+# t = DownloadProgressBar(...)
+# try:
+#     urllib.request.urlretrieve(..., reporthook=t.update_to)
+# finally:
+#     t.close() # 必ず後片付け処理を呼ぶ必要がある
